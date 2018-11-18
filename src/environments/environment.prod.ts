128e74075ac6008d89e7317c174ac2cd78a5a7b7
Caleb Froese <calebfroese@Calebs-iMac.local>
2018-11-18 16:29:46 +1030

    Update to 0.0.1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,5 +5,5 @@ export const AppConfig = {
     'https://s3.amazonaws.com/boulevard-versioning-bucket/latest.json',
   releaseNotesUrl:
     'https://s3.amazonaws.com/boulevard-versioning-bucket/release-notes.html',
-  launcherVersion: '0.0.0',
+  launcherVersion: '0.0.1',
 };
